b93673c4ca0126c5671b2e1248930bcbc908078e
ZinHua-Li <setiadharma224@gmail.com>
2025-04-01 11:26:05 +0800

    belajar

---
+++ belajar_1/belajaraplikasi1/android/app/src/main/kotlin/com/example/belajaraplikasi1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.belajaraplikasi1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ belajar_1/belajaraplikasi1/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'halaman/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
